dbd6e7c2cb4127a9c72570a972207de071415b58
Neil McCarthy <nmccarthy@yammer-inc.com>
2014-11-12 23:31:47 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socmon::Application.config.secret_key_base = 'ff01f0b9a163374e89f660e77d8811e187cba3cd12389dc92c9364d5ec390a1b3bd0881b803643a7892c3ae8cbdf5b8a4098d1b10f5a113e7cec5bc509c73475'
