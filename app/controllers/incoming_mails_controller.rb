c3ae12ae1b223c3b264d76151f5bcea7157b9046
Neil McCarthy <nmccarthy@yammer-inc.com>
2014-11-13 17:59:12 -0800

    better logging

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -3,11 +3,14 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
+    Rails.logger.debug "-------------------------Envelope-------------------------"
     Rails.logger.debug params[:envelope][:to] # print the to field to the logs
+    Rails.logger.debug "-------------------------Subject-------------------------"
     Rails.logger.debug params[:subject] # print the subject to the logs
+    Rails.logger.debug "-------------------------Plain-------------------------"
     Rails.logger.debug params[:plain] # print the decoded body plain to the logs if present
+    Rails.logger.debug "-------------------------HTML-------------------------"
     Rails.logger.debug params[:html] # print the html decoded body to the logs if present
-    Rails.logger.debug params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
 
     # Do some other stuff with the mail message
 
