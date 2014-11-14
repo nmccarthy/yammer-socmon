294b6d4e7edfe06d76850624cf67b36723f4f420
Neil McCarthy <nmccarthy@yammer-inc.com>
2014-11-13 16:15:03 -0800

    added cloudmailin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Socmon::Application.routes.draw do
   root 'pages#index'
+  get 'incoming_messages' => ''
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
